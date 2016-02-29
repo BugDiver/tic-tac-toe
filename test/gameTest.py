b86390716a30431e416a077641aaa53ccf0a3efd
BugDiver <vinaysh@thoughtworks.com>
2016-02-29 18:27:06 +0530

    [Vinay]:-game object is almost ready
    * write some methods under game class.
    * also wrote some method for bot.
    
    *we have to wire up the game class with server and UI.

---
+++ test/gameTest.py
@@ -0,0 +1,88 @@
+import unittest
+
+from src.game import Game
+
+
+class TestGameTest(unittest.TestCase):
+    def test_game_should_return_user_symbol(self):
+        game = Game("X")
+        self.assertEquals(game.getUserSymbol(), 'X')
+
+    def test_isSpaceFree_should_tell_if_given_position_is_free(self):
+        game = Game('X')
+        self.assertTrue(game.isSpaceFree(1))
+        self.assertTrue(game.isSpaceFree(2))
+        self.assertTrue(game.isSpaceFree(3))
+        self.assertTrue(game.isSpaceFree(4))
+        self.assertTrue(game.isSpaceFree(5))
+        self.assertTrue(game.isSpaceFree(6))
+        self.assertTrue(game.isSpaceFree(7))
+        self.assertTrue(game.isSpaceFree(8))
+        self.assertTrue(game.isSpaceFree(9))
+
+    def test_make_move_should_make_users_move(self):
+        game = Game('X')
+        game.makeMove(1, 'X')
+        self.assertFalse(game.isSpaceFree(1))
+
+    def test_make_move_should_not_make_users_move_if_there_is_no_space(self):
+        game = Game('X')
+        game.makeMove(1, 'X')
+        game.makeMove(1, 'X')
+        self.assertFalse(game.isSpaceFree(1))
+
+    def test_isBoardFull_should_return_true_if_there_is_no_any_space_to_move(self):
+        game = Game('X')
+        game.makeMove(1, 'X')
+        game.makeMove(2, 'X')
+        game.makeMove(3, 'X')
+        self.assertFalse(game.isBoardFull())
+
+        game.makeMove(4, 'X')
+        game.makeMove(5, 'X')
+        game.makeMove(6, 'X')
+        game.makeMove(7, 'X')
+        game.makeMove(8, 'X')
+        game.makeMove(9, 'X')
+        self.assertTrue(game.isBoardFull())
+
+    def test_getBotSymbol_should_decide_symbol_for_bot_according_to_player_symbol(self):
+        game = Game('X')
+        self.assertEquals(game.getBotSymbol(), 'O')
+        game2 = Game('O')
+        self.assertEquals(game2.getBotSymbol(), 'X')
+
+    def test_isWinner_should_tell_if_given_symbol_has_won_the_game(self):
+        game = Game('X')
+        game.makeMove(1, 'X')
+        game.makeMove(2, 'X')
+        game.makeMove(3, 'X')
+
+        self.assertTrue(game.isWinner('X'))
+
+    # =============================A.I.==============================================================================
+
+    def test_getBotMove_should_try_to_move_in_middle_if_it_is_free(self):
+        game = Game('X')
+        self.assertEquals(game.getBotMove(), 5)
+
+    def test_getBotMove_should_try_to_take_a_corner_if_center_is_not_free(self):
+        game = Game('X')
+        game.makeMove(5, 'X')
+        self.assertTrue(game.getBotMove() in [1, 3, 7, 9])
+
+    def test_getBotMove_should_block_if_player_can_win_in_next_round(self):
+        game = Game('X')
+        game.makeMove(1,'X')
+        game.makeMove(3,'X')
+        self.assertEquals(game.getBotMove(),2)
+
+    def test_getBotMove_should_try_to_be_winner_if_it_can(self):
+        game = Game('X')
+        game.makeMove(1,'O')
+        game.makeMove(3,'O')
+        self.assertEquals(game.getBotMove(),2)
+
+
+if __name__ == "__main__":
+    unittest.main()
